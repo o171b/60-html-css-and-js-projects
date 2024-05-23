175ca14aef2eb314beab5365d35b464ddda95b4c
Omair <coder71717@gmail.com>
2024-05-23 10:04:23 +0400

    completed the weather app js code

--- 8. Weather App/app.js
+++ 8. Weather App/app.js
@@ -19,8 +19,22 @@ const getWeatherData = async(cityValue) => {
             throw new Error("Network response was not ok")
         };
         const data = await response.json();
-        console.log(data);
+        const temperature = Math.round(data.main.temp)
+        const description = data.weather[0].description
+        const icon = data.weather[0].icon;
+        const details = [`Feels like: ${Math.round(data.main.feels_like)}`,
+                        `Humidity: ${data.main.humidity}%`,
+                        `Wind speed: ${data.wind.speed}m/s`
+                        ]   
+        weatherDataEl.querySelector(".icon").innerHTML = `<img src=http://openweathermap.org/img/wn/${icon}.png>`;
+        weatherDataEl.querySelector(".temperature").textContent = `${temperature}°C`
+        weatherDataEl.querySelector(".description").textContent = description;
+        weatherDataEl.querySelector(".details").innerHTML = details.map((detail) => `<div>${detail}</div>`)
+        .join("");
     } catch (error){
-        console.log(error)
+        weatherDataEl.querySelector(".icon").innerHTML = "";
+        weatherDataEl.querySelector(".temperature").textContent = "";
+        weatherDataEl.querySelector(".description").textContent = "An error happened, please try again";
+        weatherDataEl.querySelector(".details").innerHTML = "";
     };
 };
