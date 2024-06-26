97e5181d1f8ca33093fef611e8230b7a41af9124
Omair <coder71717@gmail.com>
2024-06-26 22:48:02 +0400

    added html and css

---
+++ 0. test/app.js
@@ -0,0 +1,7 @@
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+numbers.map((num) => {
+    const multiply = num * 2
+    const addition = num + num
+    console.log(multiply)
+    console.log(addition)
+})
